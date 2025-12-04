c0c547d42416941d9966fc26a925c8917d8269d8
Vladimir <vlasovjb@gmail.com>
2025-12-05 01:36:08 +0300

    1)Добавлен рандомный ввод данных(будет дорабатываться) 2)Добавлен ручной ввод данных

---
+++ src/main/java/Strategy/RandomInputStrategy.java
@@ -0,0 +1,39 @@
+package Strategy;
+
+import project.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class RandomInputStrategy implements InputStrategy {
+    private final Scanner scanner;
+    private final Random random = new Random();
+
+    public RandomInputStrategy(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+
+    @Override
+    public List<Product> load() {
+        System.out.println("Сколько данных сгенерировать? : ");
+        int count = scanner.nextInt();
+        scanner.nextLine();
+
+        List<Product> productsList = new ArrayList<>();
+
+        for (int i = 0; i < count; i++) {
+            Product product = new Product.ProductBuilder()
+                    .setName("Product_" + i)
+                    .setPrice(random.nextDouble(1000))
+                    .setQuantity(random.nextInt(100))
+                    .build();
+
+            productsList.add(product);
+        }
+
+        return productsList;
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,9 +2,12 @@ package org.example;
 
 import project.MenuManager;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
         MenuManager menuManager = new MenuManager();
         menuManager.start();
     }
