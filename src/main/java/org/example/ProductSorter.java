744f67ad5abce9241907ec71a8c14390dd7f2198
AngryBird <zombieunion1115@gmail.com>
2025-12-05 17:25:57 +0300

    NewSorter
    Sorter Bubble.
    Sorter name

--- src/main/java/org/example/ProductSorter.java
+++ src/main/java/org/example/ProductSorter.java
@@ -3,12 +3,22 @@ import java.util.Comparator;
 import java.util.List;
 
 public class ProductSorter {
-    private static final Comparator<Product> PRODUCT_COMPARATOR =
-            Comparator.comparing(Product::getName)
-                    .thenComparingDouble(Product::getPrice)
-                    .thenComparingInt(Product::getQuantity);
-
-    public static void sort(List<Product> products) {
-        products.sort(PRODUCT_COMPARATOR);
+    // Сортировка по name.
+    public static void sortByName(Product[]products){
+        int n = products.length;
+        boolean swapped;
+        for (int i = 0; i < n-1; i++) {
+            swapped = false;
+            for(int j = 0; j < n - 1 - i; j++){
+                if (products[j].getName().compareTo(products[j + 1].getName()) > 0){
+                    Product temp = products[j];
+                    products[j]= products[j + 1];
+                    products[j + 1]= temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) break;
+        }
     }
+
 }
