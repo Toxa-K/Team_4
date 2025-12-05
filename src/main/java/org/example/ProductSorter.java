90dd4a645c786f2a09113048a97e99a3cf0453f5
AngryBird <zombieunion1115@gmail.com>
2025-12-05 18:02:10 +0300

    Sotrer Name, Price, Quantity
    Sotrer Name, Price, Quantity

--- src/main/java/org/example/ProductSorter.java
+++ src/main/java/org/example/ProductSorter.java
@@ -3,22 +3,62 @@ import java.util.Comparator;
 import java.util.List;
 
 public class ProductSorter {
-    // Сортировка по name.
-    public static void sortByName(Product[]products){
+    // Сортировка по Name.
+    public static void sortByName(Product[] products) {
         int n = products.length;
         boolean swapped;
-        for (int i = 0; i < n-1; i++) {
+        for (int i = 0; i < n - 1; i++) {
             swapped = false;
-            for(int j = 0; j < n - 1 - i; j++){
-                if (products[j].getName().compareTo(products[j + 1].getName()) > 0){
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (products[j].getName().compareTo(products[j + 1].getName()) > 0) {
                     Product temp = products[j];
-                    products[j]= products[j + 1];
-                    products[j + 1]= temp;
+                    products[j] = products[j + 1];
+                    products[j + 1] = temp;
                     swapped = true;
                 }
             }
             if (!swapped) break;
         }
     }
+      //Сортировка по Price.
+    public static void sortByQuantity(Product[] products) {
+        int n = products.length;
+        boolean swapped;
 
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (products[j].getPrice() > products[j + 1].getPrice()) {
+                    Product temp = products[j];
+                    products[j] = products[j + 1];
+                    products[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) break;
+
+
+        }
+    }
+
+    // Сортировка по quantity.
+    public static void sortByQuantity(Product[] products) {
+        int n = products.length;
+        boolean swapped;
+
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (products[j].getQuantity() > products[j + 1].getQuantity()) {
+                    Product temp = products[j];
+                    products[j] = products[j + 1];
+                    products[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) break;
+        }
+    }
 }
